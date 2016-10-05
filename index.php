1351715a2b74d71f22a614fcc4df61c473155351
ezu <contactforezu@gmail.com>
2016-10-05 11:07:16 -0400

    html5 base

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html><html>
+<head>
+  <meta charset="utf-8">
+  <title>Site::MMORPG Recode by contactforezu</title>
+  <link rel="stylesheet" href="app/styles/base.css">
+</head>
+<body>
+
+</body>
+</html>
