7827944605b0a0b9d73b22a40228aa53e56a2109
ezu <contactforezu@gmail.com>
2016-10-05 13:22:18 -0400

    blocked out trending and networkbar

--- index.php
+++ index.php
@@ -6,5 +6,56 @@
 </head>
 <body>
 
+<div class="trending">
+  <div class="container-fluid">
+    <div class="col-7">
+      <strong>Trending Games</strong>
+      | <a href="#" rel="nofollow">New World</a>
+      | <a href="#" rel="nofollow">World of Warcraft</a>
+      | <a href="#" rel="nofollow">Path of Exile</a>
+      | <a href="#" rel="nofollow">Paladins</a>
+    </div>
+    <div class="col-5 text-right">
+      <img class="icon" src="sprite.png" width="12" height="12" alt="">
+      <a href="#" rel="nofollow" target="_blank">Facebook</a>
+      <img  class="icon" src="sprite.png" width="12" height="12" alt="">
+      <a href="#" rel="nofollow" target="_blank">Twitter</a>
+      <img  class="icon" src="sprite.png" width="12" height="12" alt="">
+      <a href="#" rel="nofollow" target="_blank">YouTube</a>
+      <img  class="icon" src="sprite.png" width="12" height="12" alt="">
+      <a href="#" rel="nofollow" target="_blank">Twitch.tv</a>
+      <img  class="icon" src="sprite.png" width="12" height="12" alt="">
+      <a href="#" rel="nofollow" target="_blank">YouTube.Gaming</a>
+    </div>
+  </div>
+</div>
+
+<div class="networkbar">
+  <div class="container-fluid">
+    <div class="col-10">
+      <form  class="networkbar-login" name="networkbar-login" action="#" method="post">
+        <input type="text" name="username" placeholder="Enter Username...">
+        <input type="password" name="password" placeholder="Password...">
+        Remember?
+        <input type="checkbox" name="remember" checked>
+        <button type="submit">Sign-in</button>
+        <a href="#" rel="nofollow">Register Now</a>
+        | Forgot
+        <a href="#" rel="nofollow">Username</a>
+        or
+        <a href="#" rel="nofollow">Password</a>
+      </form>
+    </div>
+    <div class="col-2 text-right">
+      <a class="gamejump" href="#" rel="nofollow" target="_blank">Quick Game Jump</a>
+      <a class="rndjump" href="#" rel="nofollow" target="_blank">Random Game</a>
+    </div>
+  </div>
+</div>
+
+<div class="spacer135">
+
+</div>
+
 </body>
 </html>
